8af88a2301f810479060f9d3185407dcf2ff4ab7
Thomas Cowell <thomasc.cowell@gmail.com>
2015-10-29 23:40:39 +0300

    Cleaning out old prototype implementation

--- svn-shelve.py
+++ svn-shelve.py
@@ -220,66 +220,6 @@ def main():
     elif args.info:
         do_info( args.info )
 
-    return
-
-    cwd = os.getcwd()
-    cwd = cwd[0].upper() + cwd[1:]
-    cwd = cwd.replace( "\\", "/")
-    trace( "cwd", cwd )
-
-    svn = pysvn.Client()
-    info = svn.info( cwd )
-
-    diffContent = svn.diff( tmp_path = ".",
-        url_or_path = cwd,
-        relative_to_dir = cwd )
-
-    # TODO: adds and deletes are all just going to appear as "M" here...
-    # Probably better to use a proper svn status operation.
-    modifiedPaths = re.findall( "Index: (.*)", diffContent )
-    modifiedPaths = [ x.strip() for x in modifiedPaths ]
-    #trace( "modifiedPaths", modifiedPaths)
-
-    if len(modifiedPaths) == 0:
-        print "Nothing to shelve"
-        return
-
-    for changed in modifiedPaths:
-        print "M\t%s" % changed
-    print
-    
-    newID, storageLocation = getNewShelve()
-    meta = {
-        "id": newID,
-        "cwd": cwd,
-        "local_timestamp": time.ctime(time.time()),
-        "url": info["url"],
-        "revision": info["revision"].number,
-        "hostname": socket.gethostname(),
-        "username": getpass.getuser(),
-        "modified": modifiedPaths,
-        "message": "todo"
-    }
-
-    metaFilename = join(storageLocation, "meta")
-    patchFilename = join(storageLocation, "patch")
-    
-    file( metaFilename, "wb" ).write( pickle.dumps(meta) )
-    trace( "Wrote", metaFilename )
-    
-    file( patchFilename, "wb" ).write( diffContent )
-    trace( "Wrote", patchFilename )
-
-    print "Shelved changelist:", newID
-
-    # Revert, if requested
-    shouldRevert = "-revert" in sys.argv
-    if shouldRevert:
-        svn.revert( cwd, recurse = True )
-        print "Reverted local copy"
-
-
 
 if __name__ == "__main__":
     main()
-    
